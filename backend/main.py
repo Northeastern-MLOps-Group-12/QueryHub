ae33c01f68c174ae8d67a436ef62f3bf0e47e084
Ashwin1102 <ashwinkhairnar1102@gmail.com>
2025-10-20 17:57:15 -0400

    chnagae

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,5 @@ import uvicorn
 from backend.connectors_api import app  # adjust import to match your file1
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Cloud Run provides this1
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run provides this
     uvicorn.run(app, host="0.0.0.0", port=port)
