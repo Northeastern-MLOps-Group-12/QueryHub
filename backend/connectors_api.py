bdf524548e453d1d4fb69c5e627add5804d8be44
Ashwin1102 <ashwinkhairnar1102@gmail.com>
2025-10-21 19:09:18 -0400

    new push

--- backend/connectors_api.py
+++ backend/connectors_api.py
@@ -5,20 +5,15 @@ from .models.connector_request import ConnectorRequest
 from agents.load_data_to_vector.graph import build_graph
 from agents.load_data_to_vector.state import AgentState
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
-
+FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN")
 
 app = FastAPI(title="Connector Service API")
 
-# Enable CORS
-origins = [
-    "http://localhost:5173",  # your frontend origin
-    # you can add production domains here, e.g. "https://myfrontend.com"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,       # or ["*"] to allow all (not recommended for prod)
+    allow_origins=FRONTEND_ORIGIN,  # or ["*"] to allow all (not recommended for prod)
     allow_credentials=True,
     allow_methods=["*"],         # GET, POST, PUT, DELETE, OPTIONS
     allow_headers=["*"],         # allow all headers
